800b0d863e737f04fc706583a2ff71b76c9d27ab
NgunjiriDavid <136877456+Ngunjiri-David@users.noreply.github.com>
2026-08-25 11:16:39 +0300

    Refactor pickWord and update main function
    Refactor pickWord function to handle cases where difficulty does not exist and update main function with new test cases.

--- lib/wordpacks.dart
+++ lib/wordpacks.dart
@@ -1,15 +1,15 @@
-String? pickWord(
+/*String? pickWord(
   Map<String, List<String>> wordsByDifficulty,
   String difficulty,
 ) {
   // If difficulty exists as a key, return the first word in its list.
   // If it doesn't exist, return null.
   // 1. Build the Map 2. Iterate over the keys to search for difficulty 3. Return the answer.
-  Map<String, List<String>> wordsByDifficulty = {
+  /*Map<String, List<String>> wordsByDifficulty = {
     'Easy': ['GOLD', 'EYE', 'ONE'],
     'Medium': ['HANGMAN', 'GORGEOUS', 'ACCUMULATE'],
     'Hard': ['ITERATE', 'IRREPLICABLE', 'EPITOME'],
-  };
+  };*/
   // 2. Find difficulty:
   var foundDiff;
   if (wordsByDifficulty.containsKey(difficulty))
@@ -17,8 +17,43 @@ String? pickWord(
   for (var word in foundDiff) return word;
 }
 
-void main() {
+/*void main() {
   pickWord({
     'Easy': ['MAD', 'CON', 'DOG'],
   }, 'Easy');
+}
+*/
+*/
+void main() {
+  print(
+    pickWord({
+      'Easy': ['ZEBRA'],
+    }, 'Easy'),
+  );
+  print(
+    pickWord({
+      'Medium': ['ZIRCONIUM'],
+    }, 'Average'),
+  );
+  print(
+    pickWord({
+      //'Easy': ['Map', 'Ant', 'Sauce'],
+      'Easy': ['CAT', 'DOG', 'SUN']
+    }, 'Easy'),
+  );
+}
+
+String? pickWord(
+  Map<String, List<String>> wordByDifficulty,
+  String difficulty,
+) {
+  var foundDiff;
+  String word = '';
+  if (wordByDifficulty.containsKey(difficulty)) {
+    foundDiff = wordByDifficulty[difficulty];
+  } else {
+    return null;
+  }
+  for (var w in foundDiff) word = word + w;
+  return word;
 }
